7a87702a62df1ba29c7278a6308484b8e03737b4
amasresha <a.masresha@gmail.com>
2021-04-08 12:42:00 -0700

    added test cases and test priorities

--- Test cases.py
+++ Test cases.py
@@ -3,6 +3,8 @@ import json
 import math
 import time
 from multiprocessing import Process
+
+import pytest
 from requests_futures.sessions import FuturesSession
 import API_Requests as req
 import Helpers as hp
@@ -10,12 +12,14 @@ import Helpers as hp
 file_name = 'valid_pswd_string_only.JSON'
 
 
+@pytest.mark.run('first')
 def test_post_to_hash_accepts_valid_password():
     res = req.get_post_response(file_name)
     # assert the request is accepted
     assert res.status_code == 200 or res.status_code == 201, "Request was not successful"
 
 
+@pytest.mark.run('first')
 def test_post_to_hash_does_not_accept_empty_password():
     file_name = 'empty_string.JSON'
     res = req.get_post_response(file_name)
@@ -23,20 +27,23 @@ def test_post_to_hash_does_not_accept_empty_password():
     assert res.status_code == 400, "System did not return a 400 code for empty string password"
 
 
-def test_post_to_hash_accepts_special_characters_as_password():
+@pytest.mark.run('first')
+def test_post_to_hash_does_not_accept_some_special_characters_as_password():
     file_name = 'special_characters.JSON'
     res = req.get_post_response(file_name)
     # assert the request is accepted
     assert res.status_code == 400, "Accepts most of the special characters"
 
 
-def test_post_to_hash_accepts_long_string_as_password():
+@pytest.mark.run('first')
+def test_post_to_hash_does_not_accept_very_long_string_as_password():
     file_name = 'long_string.JSON'
     res = req.get_post_response(file_name)
     # assert the request is accepted
     assert res.status_code == 400, "System accepts more than 1100 characters string as a password"
 
 
+@pytest.mark.run('first')
 def test_post_to_hash_does_not_accept_invalid_json_file():
     file_name = 'invalid_json_file.JSON'
     res = req.get_post_response(file_name)
@@ -44,18 +51,21 @@ def test_post_to_hash_does_not_accept_invalid_json_file():
     assert res.status_code == 400, "System did not return a 400 code for empty string password"
 
 
+@pytest.mark.run('first')
 def test_post_to_hash_returns_job_identifier_immediately():
     res = req.get_post_response(file_name)
     var = res.elapsed.seconds
     assert int(var) < 1, "The job identifier is not returned in a second"
 
 
+@pytest.mark.run('first')
 def test_password_hash_is_computed_after_5_seconds():
     res = req.get_post_response(file_name)
     var = res.elapsed.seconds
     assert int(var) == 5, "the password hash is not computed at five seconds"
 
 
+@pytest.mark.run('first')
 def test_valid_sha512_hash_is_generated():
     res = req.get_post_response(file_name)
     work_id = int(res.content.decode('UTF-8'))
@@ -65,6 +75,7 @@ def test_valid_sha512_hash_is_generated():
     assert len(hashed_str) == 128, "The hashing algorithm is not SHA512."
 
 
+@pytest.mark.run('first')
 def test_get_to_hash_accepts_a_job_identifier():
     res = req.get_post_response(file_name)
     work_id = res.content.decode('UTF-8')
@@ -72,6 +83,7 @@ def test_get_to_hash_accepts_a_job_identifier():
     assert cont.status_code == 200, "the hash not accepting a job identifier"
 
 
+@pytest.mark.run('first')
 def test_function_returns_base64_encoded_password_hash():
     res = req.get_post_response(file_name)
     work_id = res.content.decode('UTF-8')
@@ -80,6 +92,7 @@ def test_function_returns_base64_encoded_password_hash():
     assert is_base_64 is True, "the password hash is not base64 encoded"
 
 
+@pytest.mark.run('first')
 def test_get_to_stats_accept_no_data():
     req.get_post_response(file_name)
     sub_url = "/stats/2"
@@ -87,6 +100,7 @@ def test_get_to_stats_accept_no_data():
     assert cont.status_code == 404, "A GET to /stats accepts data."
 
 
+@pytest.mark.run('first')
 def test_get_to_stats_returns_json_type():
     req.get_post_response(file_name)
     cont = req.get_stats()
@@ -95,6 +109,7 @@ def test_get_to_stats_returns_json_type():
     assert is_json_type, "The response is not type: 'application/json'"
 
 
+@pytest.mark.run('first')
 def test_get_to_stats_returns_correct_total_hash_requests():
     req.get_post_response(file_name)
     cont = req.get_stats()
@@ -103,6 +118,7 @@ def test_get_to_stats_returns_correct_total_hash_requests():
     assert total_req >= 1, "Stat of TotalRequests is < 1"
 
 
+@pytest.mark.run('first')
 def test_get_to_stats_returns_correct_average_time():
     req.get_post_response(file_name)
     cont = req.get_stats()
@@ -111,6 +127,7 @@ def test_get_to_stats_returns_correct_average_time():
     assert avg_time >= 5, "The average time is less than 5 seconds. "
 
 
+@pytest.mark.run('second')
 def test_shutdown():
     shut_dwn = req.shutdown()
     cont_len = len(shut_dwn.content.decode('UTF-8'))
@@ -119,6 +136,7 @@ def test_shutdown():
                         "response "
 
 
+@pytest.mark.run('second')
 def test_it_allows_in_flight_password_hashing_while_gracefully_shutdown():
     post_pr = Process(target=req.get_post_response(file_name))
     post_pr.start()
@@ -132,6 +150,7 @@ def test_it_allows_in_flight_password_hashing_while_gracefully_shutdown():
     assert (post_status_code == 200 and shut_down_status_code == 200), "Graceful shutdown is not respected"
 
 
+@pytest.mark.run('third')
 def test_it_rejects_new_requests_while_shutting_down():
     shutdwn_pr = Process(target=req.shutdown())
     post_pr = Process(target=req.get_post_response(file_name))
@@ -145,7 +164,8 @@ def test_it_rejects_new_requests_while_shutting_down():
     assert (shut_down_status_code == 200 and post_status_code == 503), "New request is allowed while shutting down"
 
 
-def test_it_supports_multiple_concurrent_post_requests():
+@pytest.mark.run('second')
+def test_app_supports_multiple_concurrent_post_requests():
     url = req.base_url() + "/hash"
     with FuturesSession() as session:
         futures = [session.post(url, data='{"password":"angrymonkey"}') for _ in range(100)]
@@ -155,6 +175,7 @@ def test_it_supports_multiple_concurrent_post_requests():
             assert val == 200, "one or more post calls are failing while running concurrently"
 
 
+@pytest.mark.run('first')
 def test_it_supports_multiple_get_requests_simultaneously():
     res = req.get_post_response(file_name)
     work_id = res.content.decode('UTF-8')
@@ -167,13 +188,15 @@ def test_it_supports_multiple_get_requests_simultaneously():
             assert val == 200, "one or more get calls are failing while running concurrently"
 
 
-def test_delete_is_not_supported():
+@pytest.mark.run('first')
+def test_delete_is_not_supported_and_proper_code_is_returned():
     res = req.get_post_response(file_name)
     work_id = res.content.decode('UTF-8')
     resp = req.delete(work_id)
-    assert resp.status_code == 405,  "delete is supported"
+    assert resp.status_code == 405, "delete is supported or proper error is not displayed"
 
 
+@pytest.mark.run('first')
 def test_proper_response_for_get_request_with_invalid_work_id():
     res = req.get_post_response(file_name)
     work_id = int(res.content.decode('UTF-8')) + 2
